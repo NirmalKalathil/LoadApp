2e8c0a026442e8c42415cdc02b01cf75b84e5d4c
Vinayak S <vinayaksukhalal@gmail.com>
2026-03-04 21:54:07 +0530

    first commit

--- home.js
+++ home.js
@@ -2,7 +2,7 @@
 const express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 
